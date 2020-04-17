aaf1c19ec3b10948eea7642a9c761be6824aee09
Dinesh <34174856+dinesh2910@users.noreply.github.com>
2020-04-17 11:12:16 -0500

    Add files via upload

---
+++ BMI Calculater/BMI Calculater/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculater
+//
+//  Created by Dinesh Danda on 4/17/20.
+//  Copyright © 2020 Dinesh Danda. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let tip: String
+    let color: UIColor
+}
